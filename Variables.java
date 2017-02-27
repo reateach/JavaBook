012bd0d2c19091368adaf0fa5f259398c1cbdc12
trekitch <trekitch@ymail.com>
2017-02-27 15:17:46 -0500

    Variables
    Variables java file

---
+++ Variables.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadoc;
+
+/**
+ *
+ * @author Trevon
+ */
+public class Variables {
+    public static  void main(String args[]){
+        
+        //Thees lines declare variables called x and y
+        int x = 4;
+        int y = 24;
+        
+        //Outputs the two varables
+        System.out.println(x);
+        System.out.println(y);
+        
+        //Declares constant variables of one and two
+        final int ONE = 40;
+        final int TWO = 50; 
+        
+        //Outputing Constants
+        System.out.println(ONE);
+        System.out.println(TWO);
+        
+    }
+}
